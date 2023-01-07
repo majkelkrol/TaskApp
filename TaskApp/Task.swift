bd04f2522580aaf385ca625214c51d6b67bbdc2f
Majkel <majkelkrol@icloud.com>
2023-01-07 11:42:00 +0100

    Add Task struct and modify tasks -> taskList

---
+++ TaskApp/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  TaskApp
+//
+//  Created by Majkel on 07/01/2023.
+//
+
+import Foundation
+
+struct Task {
+    var id: Int
+    var title: String
+    var status: Bool
+}
--- TaskApp/ViewController.swift
+++ TaskApp/ViewController.swift
@@ -11,14 +11,14 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     
-    let tasks = [
-    "Play in CyberPunk 2077",
-    "Code new app in Xcode",
-    "Buy MacStudio",
-    "Read the first book in 2023",
-    "Wash yours Tesla S Plaid",
-    "Listen some metal music",
-    "Go for a run"
+    var taskList = [
+        Task(id: 1, title: "Play in CyberPunk 2077", status: true),
+        Task(id: 2, title: "Code new app in Xcode", status: true),
+        Task(id: 3, title: "Buy MacStudio", status: false),
+        Task(id: 4, title: "Read the first book in 2023", status: true),
+        Task(id: 5, title: "Wash yours Tesla S Plaid", status: false),
+        Task(id: 6, title: "Listen some metal music", status: true),
+        Task(id: 7, title: "Go for a run", status: true)
     ]
     
     
@@ -37,12 +37,12 @@ extension ViewController: UITableViewDelegate {
 
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        tasks.count
+        taskList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = tasks[indexPath.row]
+        cell.textLabel?.text = taskList[indexPath.row].title
         return cell
     }
 }
